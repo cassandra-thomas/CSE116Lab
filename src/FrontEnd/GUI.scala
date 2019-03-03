de664aa83850f5050f42691e8cd9718955f14a09
NickSeider <nseider82@gmailcom>
2019-03-03 14:33:55 -0500

    start of front end

---
+++ src/FrontEnd/GUI.scala
@@ -0,0 +1,15 @@
+package FrontEnd
+import javafx.scene.input.{KeyCode, KeyEvent, MouseEvent}
+import scalafx.animation.AnimationTimer
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.{Circle, Rectangle, Shape}
+import scalafx.scene.{Group, Scene}
+
+
+object GUI extends JFXApp {
+  val windowWidth: Double = 5000
+  val windowHeight: Double = 5000
+
+}
